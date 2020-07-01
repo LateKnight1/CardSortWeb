a62d487dc16b4eaea702678bb7641ef70ee28c2a
Duncan McPherson <schlussan.duncel@gmail.com>
2020-07-01 08:09:18 -0600

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
+  {path: 'decks', loadChildren: () => import('./deck/deck.module').then(m => m.DeckModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
